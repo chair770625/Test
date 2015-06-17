91b6c7004c7d5b0328b58391db26037826321ed9
KevinLiu <chair7706255@hotmail.com>
2015-06-17 11:14:01 +0800

    車位上傳第一版 先將程式UI建立好

---
+++ CarUpload/草悟道車位上傳/GreenwayUpCarInfo/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GreenwayUpCarInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showOperationInfo = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // showOperationInfo
+            // 
+            this.showOperationInfo.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.showOperationInfo.FormattingEnabled = true;
+            this.showOperationInfo.ItemHeight = 20;
+            this.showOperationInfo.Location = new System.Drawing.Point(355, 179);
+            this.showOperationInfo.Name = "showOperationInfo";
+            this.showOperationInfo.Size = new System.Drawing.Size(231, 144);
+            this.showOperationInfo.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 331);
+            this.Controls.Add(this.showOperationInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox showOperationInfo;
+    }
+}
+
